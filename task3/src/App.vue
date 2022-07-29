b7c328032a76d763133f4d6458a730308aed091f
Radosław Bucki <radoslaw.bucki26@gmail.com>
2022-07-29 17:05:36 +0200

    ME-37 Remove redundant modules

--- task3/src/App.vue
+++ task3/src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div class="container">
     <MoviesSearch/>
-    <MoviesByGenres/>
-    <MoviesByActors/>
+    <MoviesBySomething header="Filmy wg gatunku" something="genres" />
+    <MoviesBySomething header="Filmy wg obsady" something="cast" />
   </div>
 </template>
 
 <script>
 import MoviesSearch from '@/components/MoviesSearch';
-import MoviesByGenres from "@/components/MoviesByGenres";
-import MoviesByActors from "@/components/MoviesByActors";
+import MoviesBySomething from "@/components/MoviesBySomething";
 
 export default {
   name: 'App',
   components: {
     MoviesSearch,
-    MoviesByGenres,
-    MoviesByActors
+    MoviesBySomething
   }
 }
 </script>
-
-<style>
-.task {
-  color:red;
-}
-
-.task-done {
-  color:lightgreen;
-}
-</style>
---
+++ task3/src/components/MoviesBySomething.vue
@@ -0,0 +1,56 @@
+<template>
+  <h1>{{ header }}</h1>
+  <div class="row">
+    <span v-for="(value, key) in Object.fromEntries(Object.entries(moviesBySomething).slice(0, counter))"
+          class="col-lg-3 col-md-4 col-sm-6 pb-5">
+      <ListDisplay :title="key" :items="value" />
+    </span>
+  </div>
+  <button class="btn btn-outline-info w-100 mt-2" @click="showMore()"
+          v-show="Object.entries(moviesBySomething).length > counter">
+    Pokaż więcej<br />
+    <i class="bi bi-chevron-down"></i>
+  </button>
+</template>
+
+<script>
+import {_} from 'vue-underscore';
+import json from "@/assets/movies.json"
+import ListDisplay from "@/components/view/List";
+
+export default {
+  name: "MoviesBySomething",
+  props: {
+    header: String,
+    something: String
+  },
+  setup(props) {
+    let moviesBySomething = {};
+    _.forEach(json, (record) => {
+      _.forEach(record[props.something], (item) => {
+        if (item in moviesBySomething) {
+          moviesBySomething[item].push(record.title);
+        } else {
+          moviesBySomething[item] = [record.title];
+        }
+      });
+    });
+    return {
+      moviesBySomething
+    }
+  },
+  components: {
+    ListDisplay
+  },
+  data() {
+    return {
+      counter: 12
+    }
+  },
+  methods: {
+    showMore() {
+      this.counter += 12;
+    }
+  }
+}
+</script>
