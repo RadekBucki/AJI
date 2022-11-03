b4f0b7358f62ef9fdce500563509f713bda477d0
Radosław Bucki <236507@edu.p.lodz.pl>
2022-11-03 21:19:21 +0100

    Add axios and first request

--- task4/frontend/src/views/HomeView.vue
+++ task4/frontend/src/views/HomeView.vue
@@ -8,11 +8,23 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue';
+import type {AxiosInstance} from 'axios';
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $axios: AxiosInstance
+  }
+}
 
 @Options({
   components: {
     HelloWorld,
   },
+  mounted() {
+    this.$axios.get('/products')
+        .then((response: Response) => console.log(response))
+        .catch((error: Error) => console.log(error))
+  }
 })
 export default class HomeView extends Vue {}
 </script>
