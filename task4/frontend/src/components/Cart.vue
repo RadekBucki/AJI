a92b6a33b5c2f3aabf0a687bec799724d46083d4
Radosław Bucki <236507@edu.p.lodz.pl>
2022-11-12 13:36:11 +0100

    Add cart view and component

---
+++ task4/frontend/src/components/Cart.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cart">
+    <h1>This is cart</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  name: "Cart"
+})
+export default class Cart extends Vue {
+}
+</script>
+
+<style scoped>
+</style>
--- task4/frontend/src/views/CartView.vue
+++ task4/frontend/src/views/CartView.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="cart">
-    <h1>This is cart</h1>
-  </div>
+  <Cart/>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Cart from '../components/Cart.vue';
+
+@Options({
+  name: "CartView",
+  components: {
+    Cart
+  }
+})
+export default class CartView extends Vue {
+}
+</script>
