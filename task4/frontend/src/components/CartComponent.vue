d3f2846d9d1aa12d57bbd54f09a35fb2342f01a0
Radosław Bucki <236507@edu.p.lodz.pl>
2022-11-12 20:33:32 +0100

    Create customer data form and valid

--- task4/frontend/src/components/CartComponent.vue
+++ task4/frontend/src/components/CartComponent.vue
@@ -31,6 +31,35 @@
       </tr>
     </tfoot>
   </table>
+
+  <form class="needs-validation" @submit.prevent="validate" id="form" novalidate
+        v-bind:class="{ 'was-validated': wasValidated }"
+        v-show="$route.name === 'cart'"
+  >
+    <h1>Dane zamawiającego</h1>
+    <div class="input-group has-validation mt-1">
+      <input id="name" v-model="name" type="text" class="form-control" placeholder="Imię"
+             v-bind:class="{ 'is-invalid': nameError }">
+      <div class="invalid-feedback" v-if="errors.name">
+        <p class="mb-0" v-for="error in errors.name">{{ error }}</p>
+      </div>
+    </div>
+    <div class="input-group has-validation mt-1">
+      <input id="email" v-model="email" type="text" class="form-control" placeholder="Email"
+             v-bind:class="{ 'is-invalid': emailError }">
+      <div class="invalid-feedback" v-if="errors.email">
+        <p class="mb-0" v-for="error in errors.email">{{ error }}</p>
+      </div>
+    </div>
+    <div class="input-group has-validation mt-1">
+      <input id="phone" v-model="phone" type="tel" class="form-control mt-1" placeholder="Numer telefonu"
+             v-bind:class="{ 'is-invalid': phoneError }">
+      <div class="invalid-feedback" v-if="errors.phone">
+        <p class="mb-0" v-for="error in errors.phone">{{ error }}</p>
+      </div>
+    </div>
+    <button class="btn btn-primary mt-2" type="submit">Złóż zamówienie</button>
+  </form>
 </template>
 
 <script lang="ts">
@@ -49,6 +78,56 @@ import {Cart} from '@/custom-types/Cart'
   },
   props: {
     cart: Cart
+  },
+  data() {
+    return {
+      wasValidated: false,
+      name: '',
+      email: '',
+      phone: '',
+      nameError: false,
+      emailError: false,
+      phoneError: false,
+      errors: {
+        name: [],
+        email: [],
+        phone: []
+      }
+    }
+  },
+  methods: {
+    validate: function () {
+      this.wasValidated = false;
+      this.nameError = false;
+      this.emailError = false;
+      this.phoneError = false;
+      this.errors = {
+        name: [],
+        email: [],
+        phone: []
+      };
+      if (!this.name.length) {
+        this.nameError = true;
+        this.errors.name.push('Imię musi być wypełnione.');
+      }
+      if (!this.email.length) {
+        this.emailError = true;
+        this.errors.email.push('Email musi być wypełniony.');
+      }
+      if ((this.email.match(/@/g) || []).length !== 1) {
+        this.emailError = true;
+        this.errors.email.push('Email musi zawierać dokładnie 1 znak @.');
+      }
+      if ((this.email.match(/./g) || []).length < 1) {
+        this.emailError = true;
+        this.errors.email.push('Email musi zawierać znak `.`.');
+      }
+      if (!this.phone.length) {
+        this.phoneError = true;
+        this.errors.phone.push('Numer telefonu musi być wypełniony.');
+      }
+      this.wasValidated = true;
+    }
   }
 })
 export default class CartComponent extends Vue {
