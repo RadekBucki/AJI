5950463651a9edbb16c9ad741ab5ace7d7b499e2
Radosław Bucki <236507@edu.p.lodz.pl>
2022-11-13 09:00:14 +0100

    Add order requests

--- task4/backend/app.js
+++ task4/backend/app.js
@@ -8,7 +8,8 @@ var cors = require('cors')
 var indexRouter = require('./routes/index');
 var productsRouter = require('./routes/products');
 var categoriesRouter = require('./routes/categories');
-var orderRouter = require('./routes/orderStatus');
+var orderStatusRouter = require('./routes/orderStatus');
+var ordersRouter = require('./routes/orders');
 
 var app = express();
 
@@ -26,7 +27,8 @@ app.use(cors())
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
 app.use('/categories', categoriesRouter);
-app.use('/status  ', orderRouter);
+app.use('/status', orderStatusRouter);
+app.use('/orders', ordersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ task4/backend/routes/orders.js
@@ -0,0 +1,178 @@
+var mysql = require('mysql');
+var express = require('express');
+const {ER_BAD_NULL_ERROR, ER_DUP_ENTRY} = require('mysql/lib/protocol/constants/errors');
+require('dotenv').config();
+
+var router = express.Router();
+
+const connectionData = {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+};
+
+router.get('/', function (req, res) {
+    const connection = mysql.createConnection(connectionData);
+    connection.connect();
+    connection.query('SELECT oi2.*, (\n' +
+        '    SELECT JSON_ARRAYAGG(JSON_OBJECT(\'sku\', p.sku, \'name\', p.name, \'price\', p.unit_price, \'quantity\', oi.quantity))\n' +
+        '    FROM order_items oi\n' +
+        '             JOIN product p on p.id = oi.product_id\n' +
+        '    WHERE order_id=oi2.id\n' +
+        ') AS products\n' +
+        'FROM order_entity oi2',
+        function (error, results) {
+            if (error) {
+                console.log(error);
+                return res.status(500).json({errors: [{message: 'Internal server error'}]});
+            } else if (results.length) {
+                results.forEach(
+                    (record) => {
+                        record.products = JSON.parse(record.products)
+                    }
+                )
+                return res.status(200).json({data: results});
+            } else {
+                return res.status(404).json({errors: [{message: 'Nie odnalaziono.'}]});
+            }
+        });
+    connection.end();
+});
+
+router.post('/', function (req, res) {
+    const requiredParams = ['products', 'buyer'];
+    const params = {...req.body};
+
+    const badRequestErrors = [];
+
+    const missedParams = requiredParams.filter(param => !Object.keys(params).includes(param));
+    if (missedParams.length > 0) {
+        badRequestErrors.push({message: 'Pominięto parametry: ' + missedParams.join()});
+    }
+
+    const redundantParams = Object.keys(params).filter(param => !requiredParams.includes(param));
+    if (redundantParams.length > 0) {
+        badRequestErrors.push({message: 'Nadmiarowe parametry: ' + redundantParams.join()});
+    }
+
+    if (params.products.length < 1){
+        badRequestErrors.push({message: 'Nie można złożyć zamówienia bez produktów.'});
+    }
+
+    if (!params.buyer.username) {
+        badRequestErrors.push({message: 'Brakująca nazwa użytkownika'});
+    }
+
+    if (!params.buyer.email) {
+        badRequestErrors.push({message: 'Brakujący email'});
+    }
+
+    if (!params.buyer.phone) {
+        badRequestErrors.push({message: 'Brakujący numer telefonu'});
+    }
+
+    if ((params.buyer.email.match(/@/g) || []).length !== 1) {
+        badRequestErrors.push({message: 'Email musi zawierać dokładnie 1 znak @.'});
+    }
+
+    if ((params.buyer.email.match(/./g) || []).length < 1) {
+        badRequestErrors.push({message: 'Email musi zawierać znak `.`.'});
+    }
+
+    if (badRequestErrors.length > 0) {
+        return res.status(400).json({errors: badRequestErrors});
+    }
+
+    const connection = mysql.createConnection(connectionData);
+    connection.connect();
+    connection.query('INSERT INTO order_entity (user_name, email, phone, order_status_id)\n' +
+        '    VALUES ("' + params.buyer.username + '", "' + params.buyer.email + '", "' + params.buyer.phone + '", ' +
+        '   (SELECT id FROM order_status WHERE code = "not_approved"))',
+        function (error, results) {
+            if (error) {
+                console.log(error);
+                const errorResponse = {code: 500, message: 'Internal server error'};
+                if (error.errno === ER_DUP_ENTRY) {
+                    errorResponse.code = 409;
+                    errorResponse.message = 'Takie zamówienie już zostało złożone.';
+                } else if (error.errno === ER_BAD_NULL_ERROR) {
+                    errorResponse.code = 400;
+                    errorResponse.message = 'Podano nieprawidłowe dane.';
+                }
+                return res.status(errorResponse.code).json({errors: [{message: errorResponse.message}]});
+            } else {
+                params.products.forEach((product) => {
+                    connection.query('INSERT INTO order_items(order_id, product_id, quantity) VALUES (' + results.insertId +
+                        ', (SELECT id FROM product WHERE sku="' + product.sku + '"), ' + product.quantity + ');',
+                        function (error) {
+                            if (error) {
+                                console.log(error);
+                                const errorResponse = {code: 500, message: 'Internal server error'};
+                                if (error.errno === ER_BAD_NULL_ERROR) {
+                                    errorResponse.code = 400;
+                                    errorResponse.message = 'Nie istnieje produkt o podanym sku.';
+                                }
+                                return res.status(errorResponse.code).json({errors: [{message: errorResponse.message}]});
+                            }
+                        });
+                })
+                return res.status(201).json({data: req.body});
+            }
+        });
+});
+
+router.put('/:orderId/:statusCode', function (req, res) {
+    var connection = mysql.createConnection(connectionData);
+    connection.connect();
+    connection.query('UPDATE order_entity ' +
+        'SET order_status_id=(SELECT id FROM order_status WHERE code="' + req.params.statusCode + '")' +
+        'WHERE id=' + req.params.orderId,
+        function (error, results) {
+            if (error) {
+                console.log(error);
+                const errorResponse = {code: 500, message: 'Internal server error'};
+                if (error.errno === ER_BAD_NULL_ERROR) {
+                    errorResponse.code = 400;
+                    errorResponse.message = 'Nie istnieje status o podanym kodzie.';
+                }
+                return res.status(errorResponse.code).json({errors: [errorResponse]});
+            } else if (results.changedRows === 0) {
+                return res.status(404).json({errors: [{message: 'Nie odnaleziono.'}]});
+            } else {
+                return res.status(200).json({data: []});
+            }
+        });
+    connection.end();
+});
+
+router.get('/status/:statusCode', function (req, res) {
+    const connection = mysql.createConnection(connectionData);
+    connection.connect();
+    connection.query('SELECT oi2.*, (\n' +
+        '    SELECT JSON_ARRAYAGG(JSON_OBJECT(\'sku\', p.sku, \'name\', p.name, \'price\', p.unit_price, \'quantity\', oi.quantity))\n' +
+        '    FROM order_items oi\n' +
+        '             JOIN product p on p.id = oi.product_id\n' +
+        '    WHERE order_id=oi2.id\n' +
+        ') AS products\n' +
+        'FROM order_entity oi2 ' +
+        'WHERE oi2.order_status_id=(SELECT id FROM order_status WHERE code="' + req.params.statusCode + '")',
+        function (error, results) {
+            if (error) {
+                console.log(error);
+                return res.status(500).json({errors: [{message: 'Internal server error'}]});
+            } else if (results.length) {
+                results.forEach(
+                    (record) => {
+                        record.products = JSON.parse(record.products)
+                    }
+                )
+                return res.status(200).json({data: results});
+            } else {
+                return res.status(404).json({errors: [{message: 'Nie odnalaziono.'}]});
+            }
+        });
+    connection.end();
+});
+
+module.exports = router;
