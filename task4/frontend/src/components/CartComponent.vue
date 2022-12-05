d9ffd67d871e7fb6f0aee2c856bcb07b874ef9a6
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2022-12-05 09:02:07 +0100

    Buyer data validation bug fixes

--- task4/frontend/src/components/CartComponent.vue
+++ task4/frontend/src/components/CartComponent.vue
@@ -38,27 +38,26 @@
         @submit.prevent="validate"
         @submit="placeOrder"
         id="form" novalidate
-        v-bind:class="{ 'was-validated': wasValidated }"
         v-show="$route.name === 'cart' && cart.items.length"
   >
     <h1>Dane zamawiającego</h1>
     <div class="input-group has-validation mt-1">
       <input id="name" v-model="name" type="text" class="form-control" placeholder="Imię"
-             v-bind:class="{ 'is-invalid': nameError }">
+             v-bind:class="{ 'is-invalid': errors.name.length !== 0 }">
       <div class="invalid-feedback" v-if="errors.name">
         <p class="mb-0" v-for="error in errors.name">{{ error }}</p>
       </div>
     </div>
     <div class="input-group has-validation mt-1">
       <input id="email" v-model="email" type="text" class="form-control" placeholder="Email"
-             v-bind:class="{ 'is-invalid': emailError }">
+             v-bind:class="{ 'is-invalid': errors.email.length !== 0 }">
       <div class="invalid-feedback" v-if="errors.email">
         <p class="mb-0" v-for="error in errors.email">{{ error }}</p>
       </div>
     </div>
     <div class="input-group has-validation mt-1">
       <input id="phone" v-model="phone" type="tel" class="form-control mt-1" placeholder="Numer telefonu"
-             v-bind:class="{ 'is-invalid': phoneError }">
+             v-bind:class="{ 'is-invalid': errors.phone.length !== 0 }">
       <div class="invalid-feedback" v-if="errors.phone">
         <p class="mb-0" v-for="error in errors.phone">{{ error }}</p>
       </div>
@@ -94,9 +93,6 @@ import {AxiosResponse} from "axios";
       name: '',
       email: '',
       phone: '',
-      nameError: false,
-      emailError: false,
-      phoneError: false,
       errors: {
         name: [],
         email: [],
@@ -109,37 +105,37 @@ import {AxiosResponse} from "axios";
   methods: {
     validate: function () {
       this.wasValidated = false;
-      this.nameError = false;
-      this.emailError = false;
-      this.phoneError = false;
       this.errors = {
         name: [],
         email: [],
         phone: []
       };
       if (!this.name.length) {
-        this.nameError = true;
         this.errors.name.push('Imię musi być wypełnione.');
       }
       if (!this.email.length) {
-        this.emailError = true;
         this.errors.email.push('Email musi być wypełniony.');
       }
       if ((this.email.match(/@/g) || []).length !== 1) {
-        this.emailError = true;
         this.errors.email.push('Email musi zawierać dokładnie 1 znak @.');
       }
       if ((this.email.match(/./g) || []).length < 1) {
-        this.emailError = true;
         this.errors.email.push('Email musi zawierać znak `.`.');
       }
       if (!this.phone.length) {
-        this.phoneError = true;
         this.errors.phone.push('Numer telefonu musi być wypełniony.');
       }
-      this.wasValidated = true;
+      if (!this.phone.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)) {
+        this.errors.phone.push('Numer telefonu musi być poprawny.');
+      }
+      if (!this.errors.phone.length && !this.errors.email.length && !this.errors.name.length) {
+          this.wasValidated = true;
+      }
     },
     async placeOrder() {
+      if (!this.wasValidated) {
+        return;
+      }
       await this.$axios.post('/orders', {
         products: this.cart.items,
         buyer: {
