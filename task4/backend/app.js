20751240c8bad0d436ed78073795097456000c14
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2022-12-06 13:00:58 +0100

    CORS bug fix

--- task4/backend/app.js
+++ task4/backend/app.js
@@ -23,7 +23,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:8080']
+}))
 
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
